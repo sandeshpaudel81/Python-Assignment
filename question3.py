167538b4b7e8fd9f9f5e0debd1c0f9adc7eca010
Sandesh Paudel <sandeshpaudel81@gmail.com>
2025-05-07 13:42:52 +1000

    handle inputs

--- question3.py
+++ question3.py
@@ -1,7 +1,6 @@
 import turtle
 
 def drawTree(left_angle, right_angle, starting_branch_length, recursion_depth, branch_reduction_factor):
-    screen = turtle.Screen()
     ttl = turtle.Turtle()
 
     ttl.left(90)
@@ -10,36 +9,6 @@ def drawTree(left_angle, right_angle, starting_branch_length, recursion_depth, b
     ttl.pendown()
 
     def branch(length, depth):
-        # if depth == 0:
-        #     return
-
-        # ttl.forward(length)
-
-        # # Save position and heading before branching
-        # pos = ttl.pos()
-        # angle = ttl.heading()
-
-        # # Left branch
-        # ttl.penup()
-        # ttl.setpos(pos)
-        # ttl.setheading(angle)
-        # ttl.pendown()
-        # ttl.left(left_angle)
-        # branch(length * branch_reduction_factor, depth - 1)
-
-        # # Right branch
-        # ttl.penup()
-        # ttl.setpos(pos)
-        # ttl.setheading(angle)
-        # ttl.pendown()
-        # ttl.right(right_angle)
-        # branch(length * branch_reduction_factor, depth - 1)
-
-        # # Return to original state (optional here)
-        # ttl.penup()
-        # ttl.setpos(pos)
-        # ttl.setheading(angle)
-        # ttl.pendown()
 
         if depth > 0: 
             ttl.forward(length)
@@ -48,13 +17,13 @@ def drawTree(left_angle, right_angle, starting_branch_length, recursion_depth, b
     
             # recursive call for 
             # the right subtree 
-            branch(branch_reduction_factor * length, depth-1) 
+            branch(int(branch_reduction_factor * length), depth-1) 
             
             ttl.left(right_angle+left_angle) 
     
             # recursive call for 
             # the left subtree 
-            branch(branch_reduction_factor * length, depth-1) 
+            branch(int(branch_reduction_factor * length), depth-1) 
             
             ttl.right(right_angle) 
             ttl.forward(-length) 
@@ -63,15 +32,79 @@ def drawTree(left_angle, right_angle, starting_branch_length, recursion_depth, b
     turtle.done()
 
 def main():
-    left_angle = float(input("Enter left branch angle:"))
-    right_angle = float(input("\nEnter right branch angle:"))
-    starting_branch_length = float(input("\nEnter the length of first branch:"))
-    recursion_depth = int(input("\nEnter no of branches to draw:"))
-    branch_reduction_percentage = float(input("\nEnter the percentage of child branch to parent branch:"))
+    # get an input and handle left branch angle
+    left_angle = input("Enter left branch angle:")
+    try:
+        float_lt_ang = float(left_angle)
+    except ValueError:
+        ascii_values = [ord(char) for char in left_angle]
+        float_lt_ang = float(sum(ascii_values))
+        print("Your input is converted to float number", float_lt_ang)
+        if type(float_lt_ang) != float:
+            float_lt_ang = 0.0
+            print("Your input is set as defalut value: ", float_lt_ang)
 
-    branch_reduction_factor = float(branch_reduction_percentage/100)
+    # get an input and handle right branch angle
+    right_angle = input("\nEnter right branch angle:")
+    try:
+        float_rt_ang = float(right_angle)
+    except ValueError:
+        ascii_values = [ord(char) for char in right_angle]
+        float_rt_ang = float(sum(ascii_values))
+        print("Your input is converted to float number: ", float_rt_ang)
+        if type(float_rt_ang) != float:
+            float_rt_ang = 0.0
+            print("Your input is set as defalut value: ", float_rt_ang)
 
-    drawTree(left_angle, right_angle, starting_branch_length, recursion_depth, branch_reduction_factor)
+    # get an input and handle starting branch length
+    starting_branch_length = input("\nEnter the length of first branch:")
+    try:
+        int_starting_length = int(starting_branch_length)
+    except ValueError:
+        try:
+            float_starting_length = float(starting_branch_length)
+            int_starting_length= int(round(float_starting_length,0))
+            print("Your input is rounded to ", int_starting_length)
+        except ValueError:
+            ascii_values = [ord(char) for char in starting_branch_length]
+            int_starting_length = round(sum(ascii_values),0)
+            print("Your input is converted to integer number: ", int_starting_length)
+            if type(int_starting_length) != int:
+                int_starting_length = 0
+                print("Your input is set as defalut value: ", int_starting_length)
+
+    # get an input and handle depth of tree
+    recursion_depth = input("\nEnter no of branches to draw:")
+    try:
+        int_recursion_depth = int(recursion_depth)
+    except ValueError:
+        try:
+            float_recursion_depth = float(recursion_depth)
+            int_recursion_depth= int(round(float_recursion_depth,0))
+            print("Your input is rounded to ", int_recursion_depth)
+        except ValueError:
+            ascii_values = [ord(char) for char in recursion_depth]
+            int_recursion_depth = round(sum(ascii_values),0)
+            print("Your input is converted to integer number: ", int_recursion_depth)
+            if type(int_recursion_depth) != int:
+                int_recursion_depth = 0
+                print("Your input is set as defalut value: ", int_recursion_depth)
+
+    # get an input and handle branch reduction percentage
+    branch_reduction_percentage = input("\nEnter the percentage of child branch with respect to parent branch:")
+    try:
+        float_branch_percent = float(branch_reduction_percentage)
+    except ValueError:
+        ascii_values = [ord(char) for char in branch_reduction_percentage]
+        float_branch_percent = float(sum(ascii_values))
+        print("Your input is converted to float number: ", float_branch_percent)
+        if type(float_branch_percent) != float:
+            float_branch_percent = 0.0
+            print("Your input is set as defalut value: ", float_branch_percent)
+
+    branch_reduction_factor = float(float_branch_percent/100)
+
+    drawTree(float_lt_ang, float_rt_ang, int_starting_length, int_recursion_depth, branch_reduction_factor)
 
 
 if __name__=="__main__":
